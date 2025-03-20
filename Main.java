e57cfeb69d67136802ad6bc7a44afd12e6fe5f17
Anushka-H <anushkah2010@gmail.com>
2025-03-19 21:03:31 -0400

    implement simple calculator class
    Signed-off-by: Anushka-H <anushkah2010@gmail.com>

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        char operator;
+        double number1, number2, result;
+
+        Scanner keyboardInput = new Scanner(System.in);
+
+        System.out.println("Enter the operator: ");
+        operator = keyboardInput.next().charAt(0);
+
+        System.out.println("Enter the number 1: ");
+        number1 = keyboardInput.nextDouble();
+        System.out.println("Enter the number 2: ");
+        number2 = keyboardInput.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+            default:
+                System.out.println("Invalid operator");
+                break;
+        }
+
+        //keyboardInput.close();
+    }
+}
